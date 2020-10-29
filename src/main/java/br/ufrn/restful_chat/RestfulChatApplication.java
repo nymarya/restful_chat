a6c5e517c61cc2af57a97e5e924ca866ee42d951
Mayra Azevedo <mayradazevedo@gmail.com>
2020-10-29 15:57:13 -0300

    cria controller exemplo

--- src/main/java/br/ufrn/restful_chat/RestfulChatApplication.java
+++ src/main/java/br/ufrn/restful_chat/RestfulChatApplication.java
@@ -3,6 +3,7 @@ package br.ufrn.restful_chat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.ufrn.restful_chat.*;
 @SpringBootApplication
 public class RestfulChatApplication {
 
--- src/main/java/br/ufrn/restful_chat/UserRepository.java
+++ src/main/java/br/ufrn/restful_chat/UserRepository.java
@@ -1,14 +1,7 @@
 package br.ufrn.restful_chat;
 
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
-public interface UserRepository extends PagingAndSortingRepository<User, Long> {
-
-	List<User> findByName(@Param("name") String name);
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
